2199b25973811b9916fc60b8f17ff694329092a9
dukeeagle <luke.igel@gmail.com>
2022-12-29 13:12:20 -0600

    purge Django and make create-t3-app

--- viewer/apps/users/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from . import models
-# Register your models here.
-admin.site.register(models.Annotation)
-admin.site.register(models.Comment)
--- viewer/apps/users/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def signup(request):
-    return render(request, "users/signup.html")
-
-def login(request):
-    return render(request, "users/login.html")
