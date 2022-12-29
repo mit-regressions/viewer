5bfc47ffba6943d36d9b133e867f96af1a8f9cbd
dukeeagle <luke.igel@gmail.com>
2022-12-29 15:01:25 -0600

    convert player to a non-working Component

--- viewer/src/pages/_app.tsx
+++ viewer/src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import 'video.js/dist/video-js.css'
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
